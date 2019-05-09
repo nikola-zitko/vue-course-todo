e3a6d0b19544df7889473dcd1fad0e3fd1d4d39c
Nikola Žitko <39881508+nikola-zitko@users.noreply.github.com>
2019-05-10 00:18:22 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+      <ToDos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import ToDos from './components/ToDos';
+
+export default {
+  name: 'app',
+  components: {
+      ToDos
+  },
+  data(){
+      return {
+        todos: [
+            {
+                id: 1,
+                title: "ToDo One",
+                completed: false
+            },
+            {
+                id: 2,
+                title: "ToDo Two",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "ToDo Three",
+                completed: false
+            }
+        ]
+      }
+  },
+  methods:{
+      deleteTodo(id){
+          this.todos = this.todos.filter(todo => todo.id !== id);
+      }
+  }
+}
+</script>
+
+<style>
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+    }
+</style>
---
+++ src/components/ToDos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <ToDoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem.vue';
+
+export default{
+    name: "ToDos",
+    components: {
+        ToDoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
