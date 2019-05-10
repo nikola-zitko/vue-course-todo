7201ac450536846859a3b3950999e503728b3e1e
Nikola Žitko <nikola.zitko@gmail.com>
2019-05-10 09:35:30 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,40 @@
 <template>
   <div id="app">
+      <Header />
+      <AddToDo v-on:add-todo="addToDo"/>
       <ToDos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header';
 import ToDos from './components/ToDos';
+import AddToDo from './components/AddToDo';
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-      ToDos
+      Header,
+      ToDos,
+      AddToDo
   },
   data(){
       return {
-        todos: [
-            {
-                id: 1,
-                title: "ToDo One",
-                completed: false
-            },
-            {
-                id: 2,
-                title: "ToDo Two",
-                completed: false
-            },
-            {
-                id: 3,
-                title: "ToDo Three",
-                completed: false
-            }
-        ]
+        todos: []
       }
   },
   methods:{
       deleteTodo(id){
           this.todos = this.todos.filter(todo => todo.id !== id);
+      },
+      addToDo(newToDo){
+          this.todos = [...this.todos, newToDo];
+      },
+      created(){
+          axios.get('https://jsonplaceholder.typicode.com/todos')
+          .then(res => this.todos = res.data)
+          .catch(err => console.log(err));
       }
   }
 }
@@ -51,5 +50,18 @@ export default {
     body{
         font-family: Arial, Helvetica, sans-serif;
         line-height: 1.4;
+    }
+
+    .btn{
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+    
+    .btn:hover{
+        background: #666;
     }
 </style>
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form @submit="addToDo">
+            <input type="text" v-model="title" name="title" placeholder="Add ToDo...">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+<script>
+import uuid from 'uuid';
+export default {
+    name: "AddToDo",
+    data(){
+        return {
+            title: ''
+        }
+    },
+    methods:{
+        addToDo(e){
+            e.preventDefault();
+            const newToDo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            // send up to parent
+            this.$emit('add-todo', newToDo);
+            this.title='';
+        }
+    }    
+}
+</script>
+<style scoped>
+    form{
+        display: flex;
+    }
+    
+    input[type="text"]{
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"]{
+        flex: 2;
+    }
+</style>
